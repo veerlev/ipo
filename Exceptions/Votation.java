f39f270333daed544fa3260b32ddded7e11748b4
veerlev <veerle.verkeerd@gmail.com>
2019-09-04 15:47:09 +0100

    Add initial Votation.java file

---
+++ Exceptions/Votation.java
@@ -0,0 +1,86 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+/*******************************************
+ * Completez le programme à partir d'ici.
+ *******************************************/
+"vote électronique"
+System.out.println("Scrutin annulé, pas de votants");
+"Répartition des électeurs "
+" -> %.1f pour cent des électeurs\n"
+/*******************************************
+ * Ne pas modifier les parties fournies
+ * pour pr'eserver les fonctionnalit'es et
+ * le jeu de test fourni.
+ * Votre programme sera test'e avec d'autres
+ * donn'ees.
+ *******************************************/
+
+class Utils {
+
+    private static final Random RANDOM = new Random();
+
+    // NE PAS UTILISER CETTE METHODE DANS LES PARTIES A COMPLETER
+    public static void setSeed(long seed) {
+        RANDOM.setSeed(seed);
+    }
+
+    // génère un entier entre 0 et max (max non compris)
+    public static int randomInt(int max) {
+        return RANDOM.nextInt(max);
+    }
+}
+
+/**
+ * Classe pour tester la simulation
+ */
+
+class Votation {
+
+    public static void main(String args[]) {
+        Utils.setSeed(20000);
+        // TEST 1
+        System.out.println("Test partie I:");
+        System.out.println("--------------");
+
+        ArrayList<Postulant> postulants = new ArrayList<Postulant>();
+        postulants.add(new Postulant("Tarek Oxlama", 2));
+        postulants.add(new Postulant("Nicolai Tarcozi", 3));
+        postulants.add(new Postulant("Vlad Imirboutine", 2));
+        postulants.add(new Postulant("Angel Anerckjel", 4));
+
+        // 30 -> nombre maximal de votants
+        // 15 jour du scrutin
+        Scrutin scrutin = new Scrutin(postulants, 30, 15, false);
+
+        scrutin.resultats();
+
+        // FIN TEST 1
+
+        // TEST 2
+        System.out.println("Test partie II:");
+        System.out.println("---------------");
+
+        scrutin = new Scrutin(postulants, 20, 15);
+        // tous les bulletins passent le check de la date
+        // les parametres de simuler sont dans l'ordre:
+        // le pourcentage de votants et le jour du vote
+        scrutin.simuler(0.75, 12);
+        scrutin.compterVotes();
+        scrutin.resultats();
+
+        scrutin = new Scrutin(postulants, 20, 15);
+        // seuls les bulletins papier non courrier passent
+        scrutin.simuler(0.75, 15);
+        scrutin.compterVotes();
+        scrutin.resultats();
+
+        scrutin = new Scrutin(postulants, 20, 15);
+        // les bulletins electroniques ne passent pas
+        scrutin.simuler(0.75, 15);
+        scrutin.compterVotes();
+        scrutin.resultats();
+        //FIN TEST 2
+
+    }
+}
